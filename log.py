848a97086cc3f9205f5698870a9d3b1f16712bd1
Cameron Hannay <114199311+cjhannay26@users.noreply.github.com>
2025-03-11 20:28:36 -0400

    Update log.py to log actions and errors

--- log.py
+++ log.py
@@ -1,2 +1,11 @@
 class Logger:
-    pass
+    def __init__(self):
+        self.audit_log = "audit.txt"
+
+    def log_action(self, action):
+        with open(self.audit_log, 'a') as f:
+            f.write(f"ACTION: {action}\n")
+
+    def log_error(self, error):
+        with open(self.audit_log, 'a') as f:
+            f.write(f"ERROR: {error}\n")
