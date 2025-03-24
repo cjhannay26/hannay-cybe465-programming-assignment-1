53c78492e9b650cc9a0c826c19b8244986df53ae
Cameron Hannay <114199311+cjhannay26@users.noreply.github.com>
2025-03-24 14:30:22 -0400

    Fix friend_management.py to correctly store friends

--- friend_management.py
+++ friend_management.py
@@ -1,11 +1,15 @@
 class FriendManager:
     def __init__(self, filename="friends.txt"):
-        self.friends = set()
+        self.friends = []
         self.filename = filename
+
+        # Clear friends.txt file each time the program is run
+        open(self.filename, 'w').close()
+        
         self.load_from_file()
 
     def add_friend(self, friend_name):
-        self.friends.add(friend_name)
+        self.friends.append(friend_name)
        
     # Return whether a someone is a friend
     def is_friend(self, friend_name):
@@ -15,12 +19,12 @@ class FriendManager:
         try:
             with open(self.filename, 'r') as file:
                 for line in file:
-                    self.friends.add(line.strip())
+                    self.friends.append(line.strip())
         except FileNotFoundError:
             print(f"Warning: {self.filename} not found. Starting with empty file {self.filename}")
 
     # Save any added friends to friends.txt
     def save_to_file(self):
-        with open("friends.txt", 'a') as f:
+        with open("friends.txt", 'w') as f:
             for friend in self.friends:
                 f.write(friend + "\n")
